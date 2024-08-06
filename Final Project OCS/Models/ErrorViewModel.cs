2a6ad2ce74090bbbd98a267246c92ad58ab3f084
Mutasem <mutasemshboul14@gmail.com>
2024-08-06 22:11:07 +0300

    Add project files.

---
+++ Final Project OCS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Final_Project_OCS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
